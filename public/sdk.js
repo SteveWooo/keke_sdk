e3d98d74f1478131d8d202de1f8858e602c5dffc
stevewooo <461437874@qq.com>
2019-01-12 21:45:15 +0800

    【sdk能用了】

--- public/sdk.js
+++ public/sdk.js
@@ -3,6 +3,7 @@ function Keke_sdk(_options) {
 	//变量参数
 	this.options = {
 		items : [], //表情元素 相当于sprite
+		imgs : {}, //图片元素哈希表
 		canvas : {},
 		ctx : {}, //操作实例
 
@@ -17,11 +18,18 @@ function Keke_sdk(_options) {
 	this.config = {
 
 	}
+	if(_options.config){
+		for(var i in _options.config){
+			that.config[i] = _options.config[i];
+		}
+	}
 
 	//类接口
+
 	var classes = {
 		text : function(_options){
 			var item = {
+				id : 0,
 				type : _options.type,
 				content : _options.content,
 				x : _options.x || 0,
@@ -32,37 +40,62 @@ function Keke_sdk(_options) {
 
 				fill_style : _options.fill_style || "#111", //颜色
 
-				font : _options.font || "48px serif", //字体样式
+				font : _options.font || "48px black", //字体样式
 			}
-
-			var t = that.options.ctx.measureText(item.content);
-			item.text_width = t.width;
 
 			return item;
 		},
 		img : function(_options){
+			if(!_options.origin_sticker_id || !_options.file_path){
+				throw {
+					message : "参数错误：file_path or origin_sticker_id"
+				}
+			}
 			var item = {
+				id : 0,
+				type : _options.type,
+				origin_sticker_id : _options.origin_sticker_id,
+				file_path : _options.file_path,
+				x : _options.x || 0,
+				y : _options.y || 0,
 
+				visible : true, //是否渲染
 			}
 
+			//web端：
+			if(!that.options.imgs[_options.origin_sticker_id]){
+				var img_dom = document.createElement("img");
+				img_dom.src = that.config.base_res_url + item.file_path;
+				img_dom.style = "display:none";
+				//load
+				document.body.appendChild(img_dom);
+				that.options.imgs[_options.origin_sticker_id] = img_dom;
+			}
 			return item;
 		}
-	}
-	this.create_item = function(_options){
-		if(!_options.type in classes){
-			throw {
-				message : "参数错误：type"
-			}
-		}
-		return classes[_options.type](_options);
 	}
 
 	//接口区域
 	/*
 	* 新增item到渲染队列
 	*/
-	this.add_item = function(_item){
+	var add_item = function(_item){
+		_item.id = that.options.items.length + 1;
 		that.options.items.push(_item);
+		return _item;
+	}
+
+	this.create_item = function(_options){
+		if(!_options.type in classes){
+			throw {
+				message : "参数错误：type"
+			}
+		}
+		var item = classes[_options.type](_options);
+		//添加到队列 并且增加一个id
+		item = add_item(item);
+
+		return item;
 	}
 
 	/*
@@ -71,14 +104,20 @@ function Keke_sdk(_options) {
 	var renders = {
 		text : function(_item){
 			var ctx = that.options.ctx;
-			// ctx.font = _item.font;
-			// ctx.fillStyle = _item.fill_style;
-			// ctx.fillText(_item.content, _item.x, _item.y, _item.text_width);
-			ctx.fillText("hello ", 0, 0);
-			console.log(ctx)
+			var canvas = that.options.canvas;
+			ctx.font = _item.font;
+			ctx.fillStyle = _item.fill_style;
+
+			//计算宽度
+			var t = that.options.ctx.measureText(_item.content);
+			_item.text_width = t.width;
+
+			ctx.fillText(_item.content, _item.x, _item.y, _item.text_width);
 		},
 		img : function(_item){
-
+			var ctx = that.options.ctx;
+			var canvas = that.options.canvas;
+			ctx.drawImage(that.options.imgs[_item.origin_sticker_id], _item.x, _item.y);
 		}
 	}
 
@@ -86,12 +125,15 @@ function Keke_sdk(_options) {
 	* 渲染item
 	*/
 	this.render = function(_options){
+		//清理整个页面：
+		that.options.ctx.fillStyle = "#eee";
+		that.options.ctx.fillRect(0, 0, that.options.canvas.width, that.options.canvas.height);
+
 		var items = that.options.items;
 		for(var i=0;i<items.length;i++){
 			if(!items[i].visible){
 				continue;
 			}
-
 			//找到渲染器
 			if(!(items[i].type in renders)){
 				console.warn("渲染类型错误：" + items[i]);
@@ -105,6 +147,43 @@ function Keke_sdk(_options) {
 			//恢复渲染状态
 			that.options.ctx.restore();
 		}
+	}
+
+	/*
+	* 更新item，传入itemid和更新内容
+	*/
+	this.update_item = function(_id, _item){
+		var items = that.options.items;
+		for(var i=0;i<items.length;i++){
+			if(items[i]["id"] == _id){
+				//update:
+				for(var k in _item){
+					items[i][k] = _item[k];
+				}
+
+				return items[i];
+			}
+		}
+
+		return undefined;
+	}
+
+	this.get_items = function(){
+		return that.options.items;
+	}
+
+	/*
+	* 设置画布宽
+	*/
+	this.set_canvas_width = function(_width){
+		that.options.canvas.width = _width;
+	}
+
+	/*
+	* 设置画布高
+	*/
+	this.set_canvas_height = function(_height){
+		that.options.canvas.height = _height;
 	}
 
 	//逻辑区域
