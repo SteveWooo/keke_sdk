2852ab12986af2391653f13334e3b34d4d7c8629
stevewooo <461437874@qq.com>
2019-01-14 20:42:01 +0800

    【修改路径】

--- startup.js
+++ startup.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
-app.use("/public", express.static("public"));
+app.use("/dist", express.static("public"));
 
-app.listen(83);
+app.listen(83, function(){
+	console.log("listened at : 83");
+});
